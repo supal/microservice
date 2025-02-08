53016df5bd8c8d278aca0babb60521a63dd1b07b
ariful <supal.iut03@gmail.com>
2025-02-08 16:59:08 +0100

    Initial setup for the catalog service

---
+++ catalog-service/src/test/java/com/supal/catalog_service/CatalogServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.supal.catalog_service;
+
+import org.junit.jupiter.api.Test;
+
+class CatalogServiceApplicationTests extends AbstractIT {
+
+    @Test
+    void contextLoads() {}
+}
---
+++ catalog-service/src/test/java/com/supal/catalog_service/TestCatalogServiceApplication.java
@@ -0,0 +1,12 @@
+package com.supal.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main)
+                .with(ContainersConfig.class)
+                .run(args);
+    }
+}
